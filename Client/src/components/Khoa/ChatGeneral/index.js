e3d7142a516828966526fb1b1cbed2cd9b41a654
khoaphan1161996 <khoa071996@gmail.com>
2021-12-19 11:25:37 +0700

    chatFakeKhoa

--- Client/src/components/Khoa/ChatGeneral/index.js
+++ Client/src/components/Khoa/ChatGeneral/index.js
@@ -1,55 +1,65 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import { io } from "socket.io-client";
 import './style.css'
 
 const ChatGeneral = () => {
+    const [username, setUsername] = useState("Khoa");
+    const [messageValue, setMessageValue] = useState("");
+    const [messageList, setMessageList] = useState([]);
+    const [isLogin, setIsLogin] = useState(true);
+    const socketRef = useRef(null);
+
+    useEffect(() => {
+        const socket = io("http://localhost:8080");
+        socketRef.current = socket;
+
+        socket.emit("NEW_USER", username);
+
+        socket.on("NEW_MESSAGE", (message) => {
+        setMessageList((prev) => [message,...prev]);
+        });
+    }, [])
+
+    const handleSendMessage = (event) => {
+        event.preventDefault();
+        if (!socketRef.current) {
+          return;
+        }
+    
+        socketRef.current.emit("NEW_MESSAGE", messageValue);
+        setMessageValue("")
+      };
+
     return (
         <>
             <div className="chat-general-screen">
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
-                <div className="chat-general-group">
-                    <p className="chat-name">Khoa: </p>
-                    <p className="chat-mess">Hi</p>
-                </div>
+                {messageList.map((message) => {
+                    return (
+                    <div
+                    key = {message.id}
+                    className={`chat-general-group ${
+                        socketRef.current.id === message.sender.socketId 
+                        ? "my-message" : ""}`}
+                        >
+                            <p className="chat-name">{message.sender.username} :</p>
+                            <p className="chat-mess">{message.content}</p>
+                    </div>
+                    );
+                })}
             </div>
-            <div className="chat-general-form">
-                <input type="text" className="chat-general-form-item"
-                    placeholder="tên hiển thị"
-                />
-                <input type="text" className="chat-general-form-item"
-                    placeholder="nội dung"
-                />
-            </div>
+            <form className={isLogin === true ? "chat-general-form" : "chat-general-form please-sign" }
+                onSubmit={handleSendMessage}>
+                <input type="text" 
+                    className="chat-general-form-item"
+                    placeholder="Nói gì đó ..."
+                    value={messageValue}
+                    onChange={(event) => {
+                        setMessageValue(event.target.value);
+                    }}
+                    />
+                <Link to={"/signin"} className="chat-general-form-sign">Đăng nhập để chat</Link>
+            </form>
         </>
     )
 }
